1553a76f6776433a20cde218ba8a370a7edd63c7
Гавриш Олександр <synertech2023@gmail.com>
2024-07-19 20:54:29 +0300

    main

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -39,7 +39,7 @@ export class FormComponent {
 
     console.log('Data being sent to the server:', postData);
 
-    this.http.post<{ message: string }>('https://synertech-back.onrender.com/syner/users', postData, {
+    this.http.post<{ message: string }>('https://synertech-back.onrender.com', postData, {
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
